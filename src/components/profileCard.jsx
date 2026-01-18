b411544f0464da1484c235c33dd0cfb9b69ded61
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-19 00:29:11 +0530

    Update profileCard.jsx

--- src/components/profileCard.jsx
+++ src/components/profileCard.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import "../style.css"
 
 function ProfileCard ({name, image, bio, bgColor}) {
-
     const cardStyle ={
         backgroundColor: bgColor,
     };
-
     return(
         <div className="profile-card" style={cardStyle}>
             <img src={image} alt={name} className="profile-image" />
             <h2 className="profile-name">{name}</h2>
             <p className="profile-bio">{bio}</p>
         </div>
-
     );
 }
 export default ProfileCard;
